032d70e843a803599e21fb9bcd9edb54af72edbd
kke0949 <kke0949@naver.com>
2017-03-29 22:42:15 +0900

    ok

---
+++ Illumiball_0322/Assets/Script/GravityController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+
+    const float Gravity = 9.81f;    //중력 가속도
+    public float gravityScale = 1.0f;   //중력의 적용상태
+    
+	void Start () {
+
+	}
+	
+	void Update () {
+
+        Vector3 vec = new Vector3();
+        if (Application.isEditor)
+        {
+            vec.x = Input.GetAxis("Horizontal");
+            vec.z = Input.GetAxis("Vertical");
+
+            //높이 방향의 판정 : z키
+            if (Input.GetKey("z"))
+                vec.y = 1.0f;
+            else
+                vec.y = -1.0f;
+        }
+        else
+        {
+            //가속도 센서의 입력을 유니티 공간의 축에 매핑한다
+            vec.x = Input.acceleration.x;
+            vec.z = Input.acceleration.z;
+            vec.y = Input.acceleration.y;
+
+        }
+        
+        //판정된 방향 값을 계산해 위/아래로 떨어지게 설정
+        Physics.gravity = Gravity * vec.normalized * gravityScale;
+    }
+}
---
+++ test_816AR_0329/Assets/ARToolKit5-Unity/Scripts/ArRotateObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArRotateObject : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Rotate(0, Time.deltaTime * 50, 0);
+        moveObject();
+   }
+    void moveObject()
+    {
+        float keyHorizontal = Input.GetAxis("Horizontal");
+        float keyVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * Time.smoothDeltaTime * keyHorizontal, Space.World);
+        transform.Translate(Vector3.up * speed * Time.smoothDeltaTime * keyVertical, Space.World);
+    }
+}
