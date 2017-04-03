bb04d90f1a3bd00faf36677573ddac64ca6b6839
kke0949 <kke0949@naver.com>
2017-04-04 02:28:22 +0900

    ok0403

--- test_816AR_0329/Assets/ARToolKit5-Unity/Scripts/ArRotateObject.cs
+++ test_816AR_0329/Assets/ARToolKit5-Unity/Scripts/ArRotateObject.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ArRotateObject : MonoBehaviour {
+    public float moveSpeed = 1000000000000001f;
+    public float turnSpeed = 1f;
 
-    public float speed = 0.5f;
+    void Start() {
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //transform.Rotate(0, Time.deltaTime * 50, 0);
+    }
+
+    void Update() {
         moveObject();
-   }
-    void moveObject()
-    {
-        float keyHorizontal = Input.GetAxis("Horizontal");
-        float keyVertical = Input.GetAxis("Vertical");
+    }
 
-        transform.Translate(Vector3.right * speed * Time.smoothDeltaTime * keyHorizontal, Space.World);
-        transform.Translate(Vector3.up * speed * Time.smoothDeltaTime * keyVertical, Space.World);
+    void moveObject() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //transform.Translate (0f, 0f, h * moveSpeed * Time.deltaTime);
+
+        //Move 
+        this.transform.Translate(0f, 0f, v * moveSpeed * Time.deltaTime);
+
+        //Turn
+        this.transform.Rotate(0f, h * turnSpeed * Time.deltaTime, 0f);
     }
 }
