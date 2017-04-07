716e3b7823d5f1497d7df06d76026f8150bd1370
kke0949 <kke0949@naver.com>
2017-04-07 21:54:08 +0900

    5.3.2

---
+++ CapstoneAR532/Assets/ARToolKit5-Unity/Scripts/ChangeMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeMenu : MonoBehaviour {
+    public void MarkMenu_change()
+    {
+        SceneManager.LoadScene("MarkScene");
+    }
+
+    public void ModelMenu_change()
+    {
+        SceneManager.LoadScene("ModelScene");
+    }
+
+    public void MainMenu_change()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void Run_change()
+    {
+        SceneManager.LoadScene("ARscene");
+    }
+}
