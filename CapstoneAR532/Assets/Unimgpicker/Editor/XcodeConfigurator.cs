9649c736fa07e1e3f64d3b3aae33edf24c8a9735
kke0949 <kke0949@naver.com>
2017-05-01 21:13:34 +0900

    0501

---
+++ CapstoneAR532/Assets/Unimgpicker/Editor/XcodeConfigurator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Diagnostics;
+using System.IO;
+using Debug = UnityEngine.Debug;
+
+namespace Kakera.Unimgpicker
+{
+    public class XcodeProjectConfigurator
+    {
+        [PostProcessBuild]
+        static void OnPostprocessBuild(BuildTarget buildTarget, string buildPath)
+        {
+            if (buildTarget != BuildTarget.iOS)
+                return;
+
+            ConfigurePlist(buildPath, "Info.plist");
+        }
+
+        static void ConfigurePlist(string buildPath, string plistPath)
+        {
+            var path = Path.Combine(buildPath, plistPath);
+
+            var descriptionFilePath = Path.Combine(Application.dataPath, "Unimgpicker/Editor/NSPhotoLibraryUsageDescription.txt");
+            if (!File.Exists(descriptionFilePath))
+            {
+                Debug.LogError(string.Format("[Unimgpicker]:File {0} is not found.", descriptionFilePath));
+                return;
+            }
+
+            var description = File.ReadAllText(descriptionFilePath);
+            
+            Debug.Log(string.Format("[Unimgpicker]:Set NSPhotoLibraryUsageDescription as \"{0}\"", description));
+        }
+    }
+}
